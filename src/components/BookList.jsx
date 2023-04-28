18c7cb6392acfb816072bec43f8ce1b61572001c
dilarademirhan <dilara.demirhan@stu.fsm.edu.tr>
2023-04-28 23:10:36 +0300

    Hide API key

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -3,14 +3,13 @@ import axios from 'axios'
 import Book from './Book'
 import SearchBox from './SearchBox'
 
-
 const BookList = () => {
   const [books, setBooks] = useState([])
   const [search, setSearch] = useState('')
 
   const fetchBooks = (event) => {
-    if(event.key === "Enter"){
-        axios.get('https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=AIzaSyABJjcP1ifyudoDRSRyO_GFtjqcNvbNgN0')
+    if(event.key === 'Enter'){
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=' + search + '&key=' + import.meta.env.VITE_API_KEY)
         .then(response => setBooks(response.data.items))
         .catch(error => console.log(error))
     }
